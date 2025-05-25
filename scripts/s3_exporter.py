85f7dd0ba5677519da9d40e094e303ff07fa46c8
matheusjorge <mat.jorge1@gmail.com>
2025-05-25 19:47:03 -0300

    feature: s3 exporter with compression

--- scripts/s3_exporter.py
+++ scripts/s3_exporter.py
@@ -11,7 +11,7 @@ class TestContent(BaseModel):
     age: int
 
 
-def build_package() -> Package:
+def build_package(package_name: str) -> Package:
     files = [
         File(filename="test.json", content=TestContent(name="test", age=10)),
         File(filename="test2.json", content=TestContent(name="test2", age=20)),
@@ -30,7 +30,7 @@ def build_package() -> Package:
     folders[1].add_folder(folders[0])
 
     root = Path(".")
-    package = Package(name="test2", root=root)
+    package = Package(name=package_name, root=root)
     package.add_folder(folders[0])
     package.add_folder(folders[1])
 
@@ -41,15 +41,23 @@ def main():
     exporter = S3Exporter(
         bucket_name="delibird-test", endpoint_url="http://localhost:9000"
     )
-    package = build_package()
+    package = build_package("test")
     exporter.export(package, enforce_uniqueness=False)
-    loaded_package = exporter.load("test2")
+    loaded_package = exporter.load(package.name)
     assert loaded_package.name == package.name
-    # assert loaded_package.root == package.root
     assert loaded_package.folders == package.folders
     assert loaded_package.folders[0].files == package.folders[0].files
     assert loaded_package.folders[1].files == package.folders[1].files
     assert loaded_package.folders[1].folders == package.folders[1].folders
+
+    package = build_package("test_compressed")
+    exporter.export(package, enforce_uniqueness=False, compress=True)
+    loaded_compressed_package = exporter.load(package.name, compressed=True)
+    assert loaded_compressed_package.name == package.name
+    assert loaded_compressed_package.folders == package.folders
+    assert loaded_compressed_package.folders[0].files == package.folders[0].files
+    assert loaded_compressed_package.folders[1].files == package.folders[1].files
+    assert loaded_compressed_package.folders[1].folders == package.folders[1].folders
 
 
 if __name__ == "__main__":
--- src/delibird/exporters/s3.py
+++ src/delibird/exporters/s3.py
@@ -18,57 +18,109 @@ class S3Exporter:
         ]:
             self.s3.create_bucket(Bucket=self.bucket_name)
 
-    def export(self, package: Package, enforce_uniqueness: bool = False):
+    def _package_exists(self, package_name: str, compressed: bool = False) -> bool:
+        response = self.s3.list_objects_v2(
+            Bucket=self.bucket_name,
+            Prefix=f"{package_name}.zip" if compressed else f"{package_name}/",
+            MaxKeys=1,
+        )
+        return "Contents" in response
+
+    def export(
+        self,
+        package: Package,
+        enforce_uniqueness: bool = False,
+        compress: bool = False,
+    ):
+
+        if enforce_uniqueness:
+            if self._package_exists(package.name):
+                raise ValueError(f"Package {package.name} already exists")
+
+        package.dump()
+
+        if compress:
+            self._export_compressed(package)
+        else:
+            self._export_uncompressed(package)
+
+    def _export_compressed(self, package: Package):
+        shutil.make_archive(
+            package.root / package.name, "zip", package.root / package.name
+        )
+        try:
+            self.s3.upload_file(
+                package.root / f"{package.name}.zip",
+                self.bucket_name,
+                f"{package.name}.zip",
+            )
+        finally:
+            (package.root / f"{package.name}.zip").unlink()
+            shutil.rmtree(package.root / package.name)
+
+    def _export_uncompressed(self, package: Package):
         def _upload_folder(folder_path: Path):
             for file_path in folder_path.rglob("*"):
                 if file_path.is_file():
                     key = str(file_path.relative_to(folder_path.parent.parent))
                     self.s3.upload_file(str(file_path), self.bucket_name, key)
 
-        # checks if the package already exists
-        if enforce_uniqueness:
-            response = self.s3.list_objects_v2(
-                Bucket=self.bucket_name, Prefix=f"{package.name}/", MaxKeys=1
-            )
-            if "Contents" in response:
-                raise ValueError(f"Package {package.name} already exists")
+        try:
+            for folder in package.folders:
+                _upload_folder(package.root / package.name / folder.name)
+        finally:
+            shutil.rmtree(package.root / package.name)
 
-        package.dump()
-        for folder in package.folders:
-            _upload_folder(package.root / package.name / folder.name)
-
-        # remove temp structure
-        shutil.rmtree(package.root / package.name)
-
-    def load(self, package_name: str, temp_dir: Path = Path(".") / "tmp") -> Package:
-        response = self.s3.list_objects_v2(
-            Bucket=self.bucket_name, Prefix=f"{package_name}/", MaxKeys=1
-        )
-        if "Contents" not in response:
-            raise ValueError(f"Package {package_name} does not exist")
-
-        response = self.s3.list_objects_v2(
-            Bucket=self.bucket_name, Prefix=f"{package_name}/"
-        )
-        if "Contents" not in response:
+    def load(
+        self,
+        package_name: str,
+        temp_dir: Path = Path(".") / "tmp",
+        compressed: bool = False,
+    ) -> Package:
+        if not self._package_exists(package_name, compressed=compressed):
             raise ValueError(f"Package {package_name} does not exist")
 
         # Create temp directory to store downloaded files
         temp_dir = temp_dir
         temp_dir.mkdir(exist_ok=True)
 
-        # Download all files
-        for file in response["Contents"]:
-            file_path = Path(file["Key"])
-            (temp_dir / file_path.parent).mkdir(parents=True, exist_ok=True)
-            self.s3.download_file(
-                self.bucket_name, file["Key"], str(temp_dir / file_path)
-            )
+        if compressed:
+            self._download_compressed_package(package_name, temp_dir)
+        else:
+            files = self._get_package_files(package_name)
+            self._download_uncompressed_package(files, temp_dir)
 
         # Load package from downloaded files
         package = Package.load(temp_dir / package_name)
 
-        # Clean up temp directory
+        if compressed:
+            (temp_dir / f"{package_name}.zip").unlink()
+
         shutil.rmtree(temp_dir / package_name)
 
         return package
+
+    def _get_package_files(self, package_name: str) -> list[str]:
+        response = self.s3.list_objects_v2(
+            Bucket=self.bucket_name, Prefix=f"{package_name}/"
+        )
+        return [file["Key"] for file in response["Contents"]]
+
+    def _download_compressed_package(
+        self, package_name: str, temp_dir: Path = Path(".") / "tmp"
+    ) -> Package:
+        self.s3.download_file(
+            self.bucket_name,
+            f"{package_name}.zip",
+            str(temp_dir / f"{package_name}.zip"),
+        )
+        shutil.unpack_archive(temp_dir / f"{package_name}.zip", temp_dir / package_name)
+
+    def _download_uncompressed_package(
+        self, files: list[str], temp_dir: Path = Path(".") / "tmp"
+    ) -> Package:
+        # Download all files
+        for file in files:
+            file_path = Path(file)
+            (temp_dir / file_path.parent).mkdir(parents=True, exist_ok=True)
+            self.s3.download_file(self.bucket_name, file, str(temp_dir / file_path))
